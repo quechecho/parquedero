b9f367475d21ee846c2842fd048df730432e1e80
quechecho <maryluzcervantesarroyo@gmail.com>
2025-08-30 12:44:44 -0500

    Add files via upload

---
+++ ejemplo lenguaje de programacion/ejemplo-lenguaje-de-programacion/src/main/java/com/uniregminton/arnulfo/ejemplo/lenguaje/de/programacion/EjemploLenguajeDeProgramacionApplication.java
@@ -0,0 +1,13 @@
+package com.uniregminton.arnulfo.ejemplo.lenguaje.de.programacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjemploLenguajeDeProgramacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjemploLenguajeDeProgramacionApplication.class, args);
+	}
+
+}
